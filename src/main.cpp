6031b7d136062298bf16eb9f30d6358fb09c91c0
TPONG <tuanphong01112002@gmail.com>
2022-12-13 22:12:49 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@
 #include "ESPAsyncWebServer.h"
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
-
+#include <Servo.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
+Servo myservo;
+int pos = 0;
+String id;
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -16,8 +20,8 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 // Replace with your network credentials
-const char *ssid = "TWM";
-const char *password = "11111111";
+const char *ssid = "NNE";
+const char *password = "01112002";
 
 #define DHTPIN 27 // Digital pin connected to the DHT sensor
 
@@ -31,8 +35,8 @@ const int Horn = 13; // set pin out horn
 
 // Uncomment the type of sensor in use:
 #define DHTTYPE DHT11 // DHT 11
-//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+// #define DHTTYPE    DHT22     // DHT 22 (AM2302)
+// #define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -47,6 +51,18 @@ String readDHTTemperature()
   // Read temperature as Fahrenheit (isFahrenheit = true)
   // float t = dht.readTemperature(true);
   // Check if any reads failed and exit early (to try again).
+  //set the value of the warning temperture
+  if (t > 30)
+  {
+    digitalWrite(Horn, 0);
+    delay(1000);
+  }
+  else
+  {
+    digitalWrite(Horn, 1);
+    delay(1000);
+  }
+  //on off the horn
   if (isnan(t))
   {
     Serial.println("Failed to read from DHT sensor!");
@@ -83,6 +99,14 @@ String readMQ2()
   percen = Value / 40.95;
   Serial.print("MQ2 value = ");
   Serial.println(Value);
+  if (Value > 3000)
+  {
+    myservo.write(90);
+  }
+  else
+  {
+    myservo.write(0);
+  }
   return String(percen);
 }
 
@@ -103,6 +127,17 @@ String readTremors()
   Value = analogRead(Tremors);
   float percen = 0;
   percen = Value / 40.95;
+  //confige the value of the warning
+  if (percen > 70)
+  {
+    digitalWrite(Horn, 0);
+    delay(1000);
+  }
+  else
+  {
+    digitalWrite(Horn, 1);
+    delay(1000);
+  }
   Serial.print("Tremors value = ");
   Serial.println(Value);
   return String(percen);
@@ -119,10 +154,10 @@ String Warning()
   tremors_value = analogRead(Tremors);
 
   float percen = 0;
-  percen = (mq2_value + sound_value + tremors_value) / 3 * 40.95;
+  percen = (mq2_value) / 40.95;
   Serial.print("Warning value = ");
   Serial.println(percen);
-  if (percen > 60000)
+  if (percen > 60)
   {
     digitalWrite(Horn, 0);
     delay(1000);
@@ -182,12 +217,6 @@ const char index_html[] PROGMEM = R"rawliteral(
     <sup class="units">&percnt;</sup>
   </p>
 
-  <p>
-    <i class="fas fa-waveform"></i>
-    <span class="dht-labels">Muc do am thanh </span>
-    <span id="sound">%SOUND%</span>
-    <sup class="units">&percnt;</sup>
-  </p>
 
   <p>
     <i class="fas fa-exclamation-circle"></i>
@@ -312,6 +341,8 @@ void setup()
   // Serial port for debugging purposes
   Serial.begin(9600);
 
+  myservo.attach(26); // SET PIN SERVO
+
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
@@ -334,6 +365,7 @@ void setup()
 
   // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
+  id = String(WiFi.localIP());
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
@@ -382,10 +414,6 @@ void loop()
     display.setTextSize(1);
     display.setTextColor(WHITE);
     display.setCursor(0, 0);
-    display.print("Muc am thanh = ");
-    display.setCursor(90, 0);
-    display.print(readSound());
-    display.setCursor(0, 20);
     display.print("Muc rung chan = ");
     display.setCursor(100, 20);
     display.print(readTremors());
